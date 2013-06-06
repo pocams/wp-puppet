877660c6d3d342016a09278be4d2b8d2fe789cfd
Mark Johnston <git@mhj.ca>
2013-06-05 21:18:23 -0500

    Only try to copy zoneinfo files in once the chroot dir exists

--- modules/wphost/manifests/chroot.pp
+++ modules/wphost/manifests/chroot.pp
@@ -4,11 +4,13 @@ define wphost::chroot ($hostname=$title, $username) {
   file { ["/home/${username}/tmp", "/home/${username}/bin", "/home/${username}/usr",
           "/home/${username}/usr/share"]:
     ensure => directory,
-    mode => 2770
+    mode => 2770,
+    before => Exec["rsync /home/${username}/usr/share/zoneinfo"]
   }
 
   # Is there a better way to handle this?
-  exec { "/usr/bin/rsync -a --delete /usr/share/zoneinfo/ /home/${username}/usr/share/zoneinfo/":
+  exec { "rsync /home/${username}/usr/share/zoneinfo":
+    command => "/usr/bin/rsync -a --delete /usr/share/zoneinfo/ /home/${username}/usr/share/zoneinfo/",
     creates => "/home/${username}/usr/share/zoneinfo/zone.tab"
   }
 
